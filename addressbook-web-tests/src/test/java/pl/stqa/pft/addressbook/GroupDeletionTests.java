9bd63412b47f0dc957c37c6dfdaa768bf66383a9
Dolodosia <dolodosia@o2.pl>
2017-11-09 09:27:03 +0100

    Group deletion test and TestBase

---
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/GroupDeletionTests.java
@@ -0,0 +1,18 @@
+package pl.stqa.pft.addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTests extends TestBase {
+
+    @Test
+    public void testGroupDeletion() {
+
+        goToGroupPage();
+        selectGroup();
+        deleteSelectedGroups();
+        returnToGroupPage();
+    }
+
+
+}
+
