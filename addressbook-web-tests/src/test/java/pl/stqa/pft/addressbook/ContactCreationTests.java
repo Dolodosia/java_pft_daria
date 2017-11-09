1b16e349a61cb6ed5d330243b013b5b5e4be664e
Dolodosia <dolodosia@o2.pl>
2017-11-09 07:55:04 +0100

    Create new contact Refactoring add Group data class

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/ContactCreationTests.java
@@ -39,27 +39,27 @@ public class ContactCreationTests {
 
 
     goToNewContact();
-    fillInNewContactForm("StefanX", "Tester", "666000666", "teststefan@gmail.com");
+    fillInNewContactForm(new ContactGroupData("Tester", "Refactor", "666000777", "refactor@gmail.com"));
   }
 
-  private void fillInNewContactForm(String firstname, String lastname, String phone, String email) {
+  private void fillInNewContactForm(ContactGroupData contactGroupData) {
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(firstname);
+    wd.findElement(By.name("firstname")).sendKeys(contactGroupData.getFirstname());
     wd.findElement(By.name("middlename")).click();
     wd.findElement(By.name("middlename")).sendKeys("\\9");
     wd.findElement(By.name("lastname")).click();
     wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(lastname);
+    wd.findElement(By.name("lastname")).sendKeys(contactGroupData.getLastname());
     wd.findElement(By.name("theform")).click();
     new Actions(wd).doubleClick(wd.findElement(By.xpath("//div[@id='content']//label[.='Telephone']"))).build().perform();
     wd.findElement(By.name("home")).click();
     wd.findElement(By.name("mobile")).click();
     wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys(phone);
+    wd.findElement(By.name("mobile")).sendKeys(contactGroupData.getPhone());
     wd.findElement(By.name("email")).click();
     wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(email);
+    wd.findElement(By.name("email")).sendKeys(contactGroupData.getEmail());
     wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
   }
 
