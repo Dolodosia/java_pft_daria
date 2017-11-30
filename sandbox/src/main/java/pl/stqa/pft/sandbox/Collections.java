cd346397e03fcb13d9597dcc3316d4de5ae66683
Dolodosia <dolodosia@o2.pl>
2017-11-30 08:00:56 +0100

    Collections

---
+++ sandbox/src/main/java/pl/stqa/pft/sandbox/Collections.java
@@ -0,0 +1,21 @@
+package pl.stqa.pft.sandbox;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Collections {
+
+  public static void main (String[] args){
+
+    String[] langs= {"java","C#","Python","PHP"};
+
+    List<String> languages = Arrays.asList("java","C#","Python","PHP");
+
+for(String l: langs){
+  System.out.println("chce nauczyc sie " + l);
+}
+    
+
+  }
+}
