0a88b5418f6ceabdbd703d49c205170193d1dabd
Dolodosia <dolodosia@o2.pl>
2017-12-06 22:26:17 +0100

    zad 12 address

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/AddressTests.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/AddressTests.java
@@ -2,14 +2,13 @@ package pl.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
 import pl.stqa.pft.addressbook.model.ContactData;
-import java.util.Arrays;
-import java.util.stream.Collectors;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class AddressTests extends TestBase  {
 
+  //zad 12
   @Test
   public void testAddress() {
     app.goTo().goToHomePage();
