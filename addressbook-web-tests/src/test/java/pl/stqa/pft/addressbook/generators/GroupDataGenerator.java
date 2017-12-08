950b2063509bfaecbce67ab6f4aaf1e8eed83fdb
Dolodosia <dolodosia@o2.pl>
2017-12-09 00:53:44 +0100

    L6 F7 JSon for Groups

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -4,6 +4,8 @@ package pl.stqa.pft.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.thoughtworks.xstream.XStream;
 import pl.stqa.pft.addressbook.model.GroupData;
 import java.io.File;
@@ -47,10 +49,25 @@ public class GroupDataGenerator {
       saveAsCsv(groups, new File(file));
     } else if (format.equals("xml")) {
       saveAsXml(groups, new File(file));
-    } else {
-      System.out.println("Unrecognised format " + format);
     }
+      else if (format.equals("json")) {
+        saveAsJson(groups, new File(file));
+      } else {
+        System.out.println("Unrecognised format " + format);
+      }
+    }
+
+
+
+
+  private void saveAsJson(List<GroupData> groups, File file) throws IOException {
+    Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+    String json = gson.toJson(groups);
+    Writer writer = new FileWriter(file);
+    writer.write(json);
+    writer.close();
   }
+
 
   private void saveAsXml(List<GroupData> groups, File file) throws IOException{
     XStream xstream = new XStream();
@@ -79,7 +96,9 @@ public class GroupDataGenerator {
     return groups;
     }
 
-
-
-
 }
+
+
+
+
+
--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,7 @@
 package pl.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -19,24 +21,37 @@ public class GroupCreationTests extends TestBase {
 
 
  @DataProvider
- public Iterator<Object[]> validGroups() throws IOException {
-             //odczyt z pliku
-             BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
-             String xml = "";
-             String line = reader.readLine();
-             while (line != null) {
-                   xml += line;
-
-                   line = reader.readLine();
+ public Iterator<Object[]> validGroupsFromXml() throws IOException {
+   BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+   String xml = "";
+   String line = reader.readLine();
+      while (line != null) {
+        xml += line;
+        line = reader.readLine();
                }
-             XStream xstream = new XStream();
-             xstream.processAnnotations(GroupData.class);
-           List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
-          return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+   XStream xstream = new XStream();
+   xstream.processAnnotations(GroupData.class);
+   List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
+   return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
           }
 
+  @DataProvider
+  public Iterator<Object[]> validGroupsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.json")));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null) {
+      json += line;
+      line = reader.readLine();
+    }
 
-  @Test(dataProvider = "validGroups")
+    Gson gson = new Gson();
+    List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>(){}.getType());//List<GroupData>.class
+    return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+  }
+
+
+  @Test(dataProvider = "validGroupsFromJson")
 public void testGroupCreation(GroupData group) {
    app.goTo().groupPage();
 
